7ce521e2ca39ed460f2ae7d35fa3d8a9bb00683a
Brandon Phillips <brandon.phillips976@gmail.com>
2021-01-23 20:26:34 -0800

    removed some boiler plate files and added basic hero

---
+++ src/components/Hero/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+export default function Hero() {
+    return (
+        <div className='hero'>
+            <img src='./assets/img/capitol-far.jpg'/>
+            <div className='text-wrapper'>
+                <h1>FBI<br />WANTED</h1>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../../components/Hero'
+import Filters from '../../components/Filters'
+import WantedList from '../../components/WantedList'
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <WantedList />
+        </>
+    )
+}
