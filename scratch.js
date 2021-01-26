520afd413c63e93a993d09de8d3349ef8d4985fb
Brandon Phillips <brandon.phillips976@gmail.com>
2021-01-26 14:58:28 -0800

    added cards for wanted people with image and basic info

--- scratch.js
+++ scratch.js
@@ -1,49 +1,56 @@
-const axios = require('axios')
+// const axios = require('axios')
 
-var subjects = []
+// var subjects = []
 
-function getSubjects(page) {
-    if (page === 18) {
-        console.log(subjects)
-        return true
-    } else {
-        return axios.get('https://api.fbi.gov/@wanted?pageSize=50&page=' + page)
-            .then(response => {
-                console.log(response.data.items.length)
+// function getSubjects(page) {
+//     if (page === 18) {
+//         console.log(subjects)
+//         return true
+//     } else {
+//         return axios.get('https://api.fbi.gov/@wanted?pageSize=50&page=' + page)
+//             .then(response => {
+//                 console.log(response.data.items.length)
 
-                response.data.items.forEach(person => {
-                    person.subjects.forEach(subject => {
-                        if (!subjects.includes(subject)) {
-                            subjects.push(subject)
-                        }
-                    })
-                })
-                console.log('page ' + page + ' done')
-                getSubjects(page + 1)
-            })
-    }
-}
+//                 response.data.items.forEach(person => {
+//                     person.subjects.forEach(subject => {
+//                         if (!subjects.includes(subject)) {
+//                             subjects.push(subject)
+//                         }
+//                     })
+//                 })
+//                 console.log('page ' + page + ' done')
+//                 getSubjects(page + 1)
+//             })
+//     }
+// }
 
-var locations = []
+// var locations = []
 
-function getLocations(page) {
-    if (page === 18) {
-        console.log(locations)
-        return true
-    } else {
-        return axios.get('https://api.fbi.gov/@wanted?pageSize=50&page=' + page)
-            .then(response => {
-                response.data.items.forEach(person => {
-                    console.log(person.locations)
-                    if (person.locations) {
-                        locations.push(person.location)
-                    }
-                })
-                console.log('page ' + page + ' done')
-                getLocations(page + 1)
-            })
-    }
-}
-getLocations(1)
+// function getLocations(page) {
+//     if (page === 18) {
+//         console.log(locations)
+//         return true
+//     } else {
+//         return axios.get('https://api.fbi.gov/@wanted?pageSize=50&page=' + page)
+//             .then(response => {
+//                 response.data.items.forEach(person => {
+//                     console.log(person.locations)
+//                     if (person.locations) {
+//                         locations.push(person.location)
+//                     }
+//                 })
+//                 console.log('page ' + page + ' done')
+//                 getLocations(page + 1)
+//             })
+//     }
+// }
+// getLocations(1)
 
-// getSubjects(1)
+// // getSubjects(1)
+
+let str = "<p>Hello<p> there my</p> friend</p>"
+str = str.replace(/<p>/g, '')
+str = str.replace(/<\/p>/g, '')
+// str = str.replace(/<p>/g, '')
+
+console.log(str)
--- src/components/WantedList/index.js
+++ src/components/WantedList/index.js
@@ -5,7 +5,55 @@ export default function WantedList(props) {
     return (
         <div className='wanted-list-wrapper'>
             {props.wantedPeople.map(person => {
-                
+                const { 
+                    images, 
+                    title, 
+                    description, 
+                    reward_max, 
+                    reward_min, 
+                    reward_text,
+                    remarks, 
+                    sex, 
+                    subjects, 
+                    warning_message 
+                } = person
+
+                // make name capitalized
+                const nameArr = title.split(' ')
+                const newNameArr = nameArr.map(name => {
+                    // make name lowercase, then split chars
+                    let splitName = name.toLowerCase().split('')
+                    // make first letter uppercase, then return joined array
+                    splitName[0] = splitName[0].toUpperCase()
+                    return splitName.join('')
+                })
+                // join names back together
+                const name = newNameArr.join(' ')
+
+                // if there are remarks, remove <p> and </p> from it's string
+                if (remarks) {
+                    var modifiedRemarks = remarks.replace(/<p>/g, '')
+                    modifiedRemarks = modifiedRemarks.replace(/<\/p>/g, '')
+                }
+
+                return <div className='person-card'>
+                    <div className='img-wrapper'>
+                        <img 
+                            src={images[0].original || images[0].large || images[0].thumb} 
+                            alt={`Image of wanted person ${title}`} 
+                        />
+                        <i className='fas fa-search-plus'></i>
+                    </div>
+                    <div className='info-wrapper'>
+                        <p className='name'>{name}</p>
+                        <p className='description'>{description}</p>
+                        <p className='reward'><strong>Reward:</strong> {!reward_max ? 'N/A' : reward_min || reward_min === 0 ? `$${reward_min} - $${reward_max}` : 'N/A'}</p>
+                        <p className='gender'><strong>Sex:</strong> {sex || 'N/A'}</p>
+                        <p className='subject'><strong>Subject(s):</strong> {subjects.join(', ') || 'N/A'}</p>
+                        <p className='warning'>{warning_message ? `WARNING: ${warning_message}` : ''}</p>
+                        <p className='remarks'>{remarks ? modifiedRemarks : ''}</p>
+                    </div>
+                </div>
             })}
         </div>
     )
